91ece210ff6bca5c1794456a9884b907926ab05b
reymark <reymarkpoquiz1@gmail.com>
2026-07-26 17:31:46 +0800

    Fix: Bria 2.0 specialized endpoints for Magic Clean

--- backend/public/flutter_bootstrap.js
+++ backend/public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3156774079" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1584862305" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
